1245731a88c8ad8f3f1d067fa28b78dc6d2afc3d
Felipe Parrondo Bastos <feliparrondobastos@gmail.com>
2021-02-08 13:58:11 -0300

    corte 08/02

--- script/index.js
+++ script/index.js
@@ -1,14 +1,31 @@
 // OBJETOS --- OBJETOS --- OBJETOS --- OBJETOS --- OBJETOS --- OBJETOS --- OBJETOS
 
-function ItemCompras (name, price, quantity, total) {
+
+// objeto reservado para almacenar los items del catalogo
+function ItemSave (pic, name, price, material, measure, id) {
+    this.pic = pic
     this.name = name
     this.price = price
-    this.quantity = quantity
-    total = price * quantity
-    this.total = total
+    this.material = material
+    this.measure = measure
+    this.id = id  //id de 5 numeros --- los primeros 2 indicando la categoria y los ultimos 3 indicando el item especifico dentro de esa categoria
 }
 
+
+//objeto reservado para guardar los items en el carrito y la información para hacerle display
+function ItemCompras (pic, name, shipp, price, quantity, total) {
+    this.pic = pic
+    this.name = name
+    this.shipp = shipp          // usado para calcular el costo de envío, se va a usar un valor númerico para calcular la distancia del cliente al almacen + tamaño del producto
+    this.price = price
+    this.quantity = quantity
+    this.total = total
+    this.id = id
+}
+
+
 // FUNCIONES --- FUNCIONES --- FUNCIONES --- FUNCIONES --- FUNCIONES --- FUNCIONES
+
 
 function compras() {
     let indicador = ""
@@ -27,7 +44,35 @@ function compras() {
     }
 }
 
+function itemSaveToCompras(ItemObject, quantity) {
+    let pic = ItemObject.pic
+    let name = ItemObject.name
+    let price = ItemObject.price
+    let measure = ItemObject.measure
+    let id = ItemObject.id
+    let shipp = measure * 3
+    let total = quantity * price
+
+
+    let newObject = new ItemCompras = (pic, name, shipp, price, quantity, total, id)
+
+    itemComprasStorage(newObject)
+
+    return newObject
+}
+
+function itemComprasStorage(newObject) {
+    let identi = toString(newObject.id) 
+    let quan = toString(newObject.quantity) 
+
+    let final = identi + "/" + quan
+    
+    sessionStorage.setItem(final)
+}
+
+
 // CODIGO --- CODIGO --- CODIGO --- CODIGO --- CODIGO --- CODIGO --- CODIGO --- CODIGO
+
 
 var newItem = []
 
